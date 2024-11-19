a385954616a5811e7e08971d27d9ca7c42bf4035
Namia-R <mk258855@outlook.com>
2024-11-20 01:56:43 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # 修改openwrt登陆地址,把下面的 192.168.10.1 修改成你想要的就可以了
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
 
 # 修改主机名字，把 OpenWrt X 修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/OpenWrt X/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/OpenWrt-RK/g' package/base-files/files/bin/config_generate
 
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
