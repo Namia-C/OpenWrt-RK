27506408f332447c01e9f63f8ce1852a7a648f3f
Namia-R <mk258855@outlook.com>
2024-11-20 17:43:49 +0800

    更新 diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -1,7 +1,7 @@
  #!/bin/bash
 
 # 修改主机名字，把 OpenWrt X 修改你喜欢的就行（不能纯数字或者使用中文）
-sed -i 's/OpenWrt/OpenWrt-RK/g' package/base-files/files/bin/config_generate
+sed -i 's/LEDE/OpenWrt-RK/g' package/base-files/files/bin/config_generate
 
 # 修改默认IP
 sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
