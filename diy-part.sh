e555fc4c5988e08bd7d4c89f1511d9d2ae57334e
Namia-R <mk258855@outlook.com>
2024-11-21 00:45:27 +0800

    Update diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -5,8 +5,7 @@ sed -i 's/LEDE/OpenWrt-RK/g' package/base-files/files/bin/config_generate
 
 # 1----------------------------------------------------------------------------------------------------------------------------------
 # 修改默认IP
-sed -i 's/192.168.1.1/192.168.10.1/g' package/base-files/files/bin/config_generate
-
+sed -i 's/192.168.1.1/192.168.10.1/g' ./package/base-files/luci2/bin/config_generate
 # 2----------------------------------------------------------------------------------------------------------------------------------
 # 更改默认 Shell 为 zsh
 # sed -i 's/\/bin\/ash/\/usr\/bin\/zsh/g' package/base-files/files/etc/passwd
