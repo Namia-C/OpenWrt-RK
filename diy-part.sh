619cef70bb624c998504612e1ddd8d6c9e1346d8
Namia-R <mk258855@outlook.com>
2024-11-21 03:47:05 +0800

    更新 diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -60,6 +60,10 @@ git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages packa
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
 git_sparse_clone master https://github.com/vernesong/OpenClash luci-app-openclash
+git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
+git clone --depth=1 https://github.com/morytyann/OpenWrt-mihomo
+
+
 
 # 8----------------------------------------------------------------------------------------------------------------------------------
 # Themes
