b300c4cee5c860325a7852a1217d76140e5c7ef0
Namia-R <mk258855@outlook.com>
2024-11-22 02:11:17 +0800

    更新 diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -59,13 +59,13 @@ git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebro
 
 # 7----------------------------------------------------------------------------------------------------------------------------------
 # 科学上网插件
+git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 git clone --depth=1 -b main https://github.com/fw876/helloworld package/luci-app-ssr-plus
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall-packages package/openwrt-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall package/luci-app-passwall
 git clone --depth=1 https://github.com/xiaorouji/openwrt-passwall2 package/luci-app-passwall2
-git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
-git clone --depth=1 https://github.com/immortalwrt/homeproxy package/luci-app-homeproxy
 git_sparse_clone main  https://github.com/morytyann/OpenWrt-mihomo luci-app-mihomo mihomo
+git_sparse_clone dev https://github.com/vernesong/OpenClash luci-app-openclash
 
 # 8----------------------------------------------------------------------------------------------------------------------------------
 # Themes
