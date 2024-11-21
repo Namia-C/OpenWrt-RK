6170d427132808a9b4bf36ceeb4e7e1048e8b73e
Namia-R <mk258855@outlook.com>
2024-11-22 00:08:58 +0800

    更新 diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -48,7 +48,7 @@ git clone --depth=1 https://github.com/sirpdboy/luci-app-netdata package/luci-ap
 git clone --depth=1 https://github.com/ouyangzq/sendat package/sendat
 git_sparse_clone main https://github.com/Lienol/openwrt-package luci-app-filebrowser luci-app-ssr-mudb-server
 # 插件包使用方法
-# git clone https://github.com/haiibo/openwrt-packages package/openwrt-packages
+# git clone --depth=1 https://github.com/haiibo/openwrt-packages package/openwrt-packages
 
 
 #git clone --depth=1 https://github.com/destan19/OpenAppFilter package/OpenAppFilter
