4d7ea8e5558a976826726d2b0eee1da37bed6d7f
Namia-R <mk258855@outlook.com>
2024-11-24 04:42:06 +0800

    更新 diy-part.sh

--- diy-part.sh
+++ diy-part.sh
@@ -8,7 +8,7 @@
 # 修改主机名字，把 OpenWrt-RK 修改你喜欢的就行（不能纯数字或者使用中文）
 sed -i 's/OpenWrt/OpenWrt-RK/g' package/base-files/files/bin/config_generate
 # 修改系统内核
-sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.12/g' target/linux/rockchip/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.6/KERNEL_PATCHVER:=6.1/g' target/linux/rockchip/Makefile
 
 # 1----------------------------------------------------------------------------------------------------------------------------------
 # 修改默认IP
